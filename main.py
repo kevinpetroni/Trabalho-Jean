50873dadbb564a115f57558ec48d7c82cca322c4
Kaylan-9 <kaylancarlos5405@gmail.com>
2023-09-18 21:53:06 -0300

    :test_tube: test

--- main.py
+++ main.py
@@ -9,28 +9,32 @@ entrada_html = """
 soup = BeautifulSoup(entrada_html, 'html.parser')
 
 # Função para imprimir uma tag e seus atributos recursivamente
-def imprimir_tag(tag, nivel):
+def print_tag(tag, nivel):
     print(f"\n{'  ' * nivel}Tag de abertura: {tag.name}, Nível {nivel}")
     
     for atributo, valor in tag.attrs.items():
         print(f"\n{'  ' * (nivel+1)}Atributo de Tag: {atributo}")
-        for i, v in enumerate(valor.split(';')):
-            partes = v.split(':')
-            if len(partes) == 2:
-                nome_css, valor_css = partes
-                print(f"\n{'  ' * (nivel+2)}Conteúdo {i + 1} do style: {nome_css.strip()}")
-                print(f"\n{'  ' * (nivel+2)}valor conteúdo {i + 1} : {valor_css.strip()}")
+        valores= enumerate(valor.split(';'))
+        if len(valores)>1:
+            for i, v in valores:
+                partes = v.split(':')
+                if len(partes) == 2:
+                    nome_css, valor_css = partes
+                    print(f"\n{'  ' * (nivel+2)}Conteúdo {i + 1} do style: {nome_css.strip()}")
+                    print(f"\n{'  ' * (nivel+2)}valor conteúdo {i + 1} : {valor_css.strip()}")
+        else:
+            print(f"\n{' ' * (nivel+1)}Valor do atributo: {valor}")
     
     if tag.string:
         print(f"\n{'  ' * (nivel+1)}conteúdo da tag: {tag.string.strip()}")
 
     for filho in tag.find_all(recursive=False):
-        imprimir_tag(filho, nivel+1)
+        print_tag(filho, nivel+1)
     
     if tag.name:
         print(f"\n{'  ' * nivel}Tag de fechamento: {tag.name}")
 
 # Iniciar a análise com a tag HTML raiz
-imprimir_tag(soup.html, 0)
+print_tag(soup.html, 0)
 
 #teste 
