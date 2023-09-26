938ce9ce16f9f8d8f4a23fbb24b50f00c73704d4
Kevin Petroni <foxyukihiro@hotmail.com>
2023-09-26 15:05:23 -0300

    feat: some updates

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
 import re
 
-# Entrada HTML
-html = """
+# Texto HTML de entrada
+html_input = """
 <html> <head> <title> Compiladores </title> </head><body> <p style="color:red;background:blue;" id="abc"> Unipinhal </p> <br> </body></html>
-""".strip()
+"""
 
-tag_name_pattern= '([^<>]+)'
-attr_name_pattern= '(\w|\d|[^<>])+'
-attr_value_pattern= '[^<>]+'
-attrs_pattern= f'(\s({attr_name_pattern})="({attr_value_pattern})")*'
-tag_pattern= rf'<{tag_name_pattern}{attrs_pattern}>(.*)<\/(\1)>'
-single_tag_pattern= rf'<{tag_name_pattern}{attrs_pattern}\/?>'
-tokens= []
+# Padrões regex para tags, atributos e conteúdo
+tag_pattern = r'<\s*(\w+)[^>]*>'
+attribute_pattern = r'(\w+)\s*=\s*["\']([^"\']+)["\']'
+content_pattern = r'>([^<]+)<'
 
-def has_closing_tag(inner_html, tag_name):
-  return re.match(rf'<\/{tag_name}>', inner_html)
+# Nível inicial
+level = 0
 
-def fetch_single_tag(inner_html, level):
-  unique_tag= ["br"]
-  has_single_tag= False
-  for match in re.finditer(single_tag_pattern, inner_html):
-    op_tag= match.group(1).strip() # tag de abertura
-    if not has_closing_tag(inner_html, op_tag):
-      for ut in unique_tag: # valida se está na lista de tags únicas que não tem terminação
-        if op_tag==ut:
-          has_single_tag= True
-          tokens.append(('Tag única', op_tag, level))
-  return has_single_tag
+# Função para imprimir uma linha formatada
+def print_line(tag, level, attribute=None, attribute_value=None, content=None):
+    indentation = " " * (level * 2)
+    if attribute is None:
+        print(f"{indentation}Tag de {'abertura' if content is not None else 'fechamento'}: {tag}, Nível {level}")
+    elif attribute_value is None:
+        print(f"{indentation}Atributo de Tag: {attribute}")
+    else:
+        print(f"{indentation}Valor atributo {attribute}: {attribute_value}")
 
+# Encontre todas as correspondências de tags, atributos e conteúdo
+for match in re.finditer(tag_pattern, html_input):
+    tag = match.group(1)
+    if '/' not in tag:
+        # Tag de abertura
+        print_line(tag, level)
+        level += 1
+    else:
+        # Tag de fechamento
+        level -= 1
+        print_line(tag[1:], level)
 
-def fetch_tag(html, level= 0):
-  count_tag= 0 # serve para impedir que o conteúdo de innerHTML que possui HTML seja impresso
-  for match in re.finditer(tag_pattern, html):
-    count_tag+=1;
-    op_tag= match.group(1).strip() # tag de abertura
-    inner_html= match.group(6).strip()
-    tokens.append(('Tag de abertura', op_tag, level))
-    has_tag= fetch_tag(inner_html, level+1)==0
-    has_single_tag= fetch_single_tag(inner_html, level+1)
-    if has_tag and not has_single_tag:
-      tokens.append(('conteúdo de tag', inner_html))
-    tokens.append(('Tag de fechamento', op_tag, level))
-  return count_tag
+    # Encontre atributos dentro da tag
+    for attribute_match in re.finditer(attribute_pattern, match.group()):
+        attribute = attribute_match.group(1)
+        attribute_value = attribute_match.group(2)
+        print_line(tag, level, attribute, attribute_value)
 
-
-fetch_tag(html)
-
-for token in tokens:
-  print(token)
+    # Encontre conteúdo dentro da tag
+    content_match = re.search(content_pattern, match.group())
+    if content_match:
+        content = content_match.group(1).strip()
+        if content:
+            print_line(tag, level, content=content)
