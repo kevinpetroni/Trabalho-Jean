c32823f7b61f5a1da8433fcd954a704eb6d8d53a
Kevin Petroni <foxyukihiro@hotmail.com>
2023-09-26 15:00:57 -0300

    feat: some updates

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
-from bs4 import BeautifulSoup
+import re
 
 # Entrada HTML
-entrada_html = """
+html = """
 <html> <head> <title> Compiladores </title> </head><body> <p style="color:red;background:blue;" id="abc"> Unipinhal </p> <br> </body></html>
-"""
+""".strip()
 
-# Analisar o HTML com BeautifulSoup
-soup = BeautifulSoup(entrada_html, 'html.parser')
+tag_name_pattern= '([^<>]+)'
+attr_name_pattern= '(\w|\d|[^<>])+'
+attr_value_pattern= '[^<>]+'
+attrs_pattern= f'(\s({attr_name_pattern})="({attr_value_pattern})")*'
+tag_pattern= rf'<{tag_name_pattern}{attrs_pattern}>(.*)<\/(\1)>'
+single_tag_pattern= rf'<{tag_name_pattern}{attrs_pattern}\/?>'
+tokens= []
 
-# Função para imprimir uma tag e seus atributos recursivamente
-def print_tag(tag, nivel):
-    print(f"\n{'  ' * nivel}Tag de abertura: {tag.name}, Nível {nivel}")
-    
-    for atributo, valor in tag.attrs.items():
-        print(f"\n{'  ' * (nivel+1)}Atributo de Tag: {atributo}")
-        valores= valor.split(';')
-        if len(valores) > 1:
-            for i, v in enumerate(valores):
-                partes = v.split(':')
-                if len(partes) == 2:
-                    nome_css, valor_css = partes
-                    print(f"\n{'  ' * (nivel+2)}Conteúdo {i + 1} do style: {nome_css.strip()}")
-                    print(f"\n{'  ' * (nivel+2)}valor conteúdo {i + 1} : {valor_css.strip()}")
-        else:
-            print(f"\n{'  ' * (nivel+1)}Valor do atributo: {valor}")
-    
-    if tag.string:
-        print(f"\n{'  ' * (nivel+1)}conteúdo da tag: {tag.string.strip()}")
+def has_closing_tag(inner_html, tag_name):
+  return re.match(rf'<\/{tag_name}>', inner_html)
 
-    for filho in tag.find_all(recursive=False):
-        print_tag(filho, nivel+1)
-    
-    if tag.name:
-        print(f"\n{'  ' * nivel}Tag de fechamento: {tag.name}")
+def fetch_single_tag(inner_html, level):
+  unique_tag= ["br"]
+  has_single_tag= False
+  for match in re.finditer(single_tag_pattern, inner_html):
+    op_tag= match.group(1).strip() # tag de abertura
+    if not has_closing_tag(inner_html, op_tag):
+      for ut in unique_tag: # valida se está na lista de tags únicas que não tem terminação
+        if op_tag==ut:
+          has_single_tag= True
+          tokens.append(('Tag única', op_tag, level))
+  return has_single_tag
 
-# Iniciar a análise com a tag HTML raiz
-print_tag(soup.html, 0)
 
-#teste 
+def fetch_tag(html, level= 0):
+  count_tag= 0 # serve para impedir que o conteúdo de innerHTML que possui HTML seja impresso
+  for match in re.finditer(tag_pattern, html):
+    count_tag+=1;
+    op_tag= match.group(1).strip() # tag de abertura
+    inner_html= match.group(6).strip()
+    tokens.append(('Tag de abertura', op_tag, level))
+    has_tag= fetch_tag(inner_html, level+1)==0
+    has_single_tag= fetch_single_tag(inner_html, level+1)
+    if has_tag and not has_single_tag:
+      tokens.append(('conteúdo de tag', inner_html))
+    tokens.append(('Tag de fechamento', op_tag, level))
+  return count_tag
+
+
+fetch_tag(html)
+
+for token in tokens:
+  print(token)
